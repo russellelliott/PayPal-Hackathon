c4137aa8373c0e39fa08ea8790a6df3e95c849ca
Russell Elliott <russell@quadsquad.net>
2024-08-20 18:36:38 -0700

    basic local stoage thing won't work. will make an api with postgres docker instead

--- server/src/utils/amount.js
+++ server/src/utils/amount.js
@@ -1,7 +1,35 @@
+//goes into localStorage and gets the total price of the items from the cart.
 function getRandomAmount() {
-  const randomNumber = (Math.random() * (200 - 10)) + 10;
+  let cart = []
+  if (typeof localStorage !== 'undefined') {
+    cart = JSON.parse(localStorage.getItem("cart")) || [];
+    console.log(cart);
+  }else{
+    console.log("uhoh")
+  }
+  //let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  let totalPrice = 0;
+  cart.forEach(item => {
+    totalPrice += item.price * item.quantity
+  });
+
+  return totalPrice.toFixed(2);
+
+  // cart.forEach(item => {
+  //   const itemDiv = document.createElement("div");
+  //   itemDiv.innerHTML = `
+  //       <p>${item.name} - Quantity: ${item.quantity}</p>
+  //       <p>Price: $${(item.price * item.quantity).toFixed(2)}</p>
+  //       <button onclick="removeFromCart(${item.id})">Remove</button>
+  //   `;
+  //   cartItemsContainer.appendChild(itemDiv);
+  //   total += item.price * item.quantity; // Add to total
+  // });
+
+  //const randomNumber = (Math.random() * (200 - 10)) + 10;
   
-  return randomNumber.toFixed(2);
+  //return randomNumber.toFixed(2);
 }
 
 module.exports = {
